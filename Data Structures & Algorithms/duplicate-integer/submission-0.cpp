fb02c52de7ad70e000c31923cd0b62abad22d2f2
Shruti bansal <120932206+shruti917@users.noreply.github.com>
2026-07-27 23:02:45 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(mp[nums[i]]>=1) return true;
+            mp[nums[i]]++;
+        }
+        return false;
+    }
+};
